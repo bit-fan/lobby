11d0a48bcc8884dfb5de14f22382fe8523d1b43d
asd <taur57@hotmail.com>
2017-04-17 23:44:24 +0800

    fix some bugs

--- a.ps1
+++
@@ -1,5 +0,0 @@
-Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force
-npm install -g npm-windows-upgrade
-npm-windows-upgrade
-npm-windows-upgrade mongodb --save -g
-npm-windows-upgrade mongoose --save -g
